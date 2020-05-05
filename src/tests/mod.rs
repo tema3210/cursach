003c4c6fcd679a07071c719fd32b5756793ea81b
tema3210 <play.aab.25122001@gmail.com>
2020-05-06 00:17:37 +0300

    Test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,8 +2,12 @@
 mod init_tests {
     #[test]
     fn test_cell(){
-        assert_eq!(crate::lib::DBCONNPOOL.get().is_some(),false)
-
-
+        assert_eq!(crate::lib::DBCONNPOOL.get().is_some(),false);
+    }
+    #[test]
+    fn initConnPool(){
+        todo!();
+        crate::lib::initConnPool(String::from(""));
+        assert_eq!(crate::lib::DBCONNPOOL.get().is_some(),true);
     }
 }
