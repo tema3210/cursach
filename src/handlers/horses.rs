561f6717b2dc60e602ea5164f9b140e9cfcef314
tema3210 <play.aab.25122001@gmail.com>
2020-05-12 18:00:21 +0300

    Add horse info handler

--- src/handlers/horses.rs
+++ src/handlers/horses.rs
@@ -1,14 +1,28 @@
 use actix_web::{web, Responder};
 use actix_web_codegen::{get};
 //use diesel::MysqlConnection;
-//use crate::{schema,lib};
+use crate::{schema,lib};
 
 
 
 #[get("/horses/info/{id}")]
 pub async fn horse_info(_info: web::Path<(u64,)>) -> impl Responder{
-	//TODO
-	"Unimplemented".with_status(http::status::StatusCode::from_u16(501).unwrap())
+	let prep_select = {
+		use schema::Horses::dsl::*;
+		Horses.filter(ID.eq(info.0))
+	}
+	let resp = lib::transaction(move |conn|{
+		prep_select.load::<lib::ORM::Run>(conn)
+	}).await;
+
+	match resp {
+		Ok(horse) => {
+			serde_json::ser::to_string(&horse).unwrap().with_status(200.try_into().unwrap())
+		},
+		Err(_) => {
+			String::from("").with_status(500.try_into().unwrap())
+		}
+	}
 }
 
 #[get("/horses/info/{idl}-{idh}")]
