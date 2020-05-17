6c34cbe527cf5d57e86dd109391ebcc62f39e7ba
tema3210 <play.aab.25122001@gmail.com>
2020-05-17 23:20:40 +0300

    make test DB init thread-safe; disable test DB init due to bad lib;

--- src/tests_framework/mod.rs
+++ src/tests_framework/mod.rs
@@ -5,6 +5,7 @@ use diesel::{
     //result::Result as DResult,
 };
 use tokio_diesel::*;
+//use crate::{lib};
 
 mod tests;
 
@@ -15,7 +16,7 @@ static dbInitFlag: OnceCell<bool> = OnceCell::new();
 pub static DBCONNPOOL: OnceCell<Pool<ConnectionManager<diesel::SqliteConnection>>> = OnceCell::new();
 
 #[cfg(test)]
-pub async fn transaction_inner<T: 'static + std::marker::Send, F>(f: F) -> std::result::Result<T, crate::lib::PoolError>
+pub async fn transaction_inner<T: 'static + std::marker::Send, F>(f: F) -> std::result::Result<T, lib::PoolError>
 where
     F: 'static + FnOnce(&diesel::SqliteConnection) -> QueryResult<T> + Send,
 {
@@ -26,16 +27,18 @@ where
     });
 
 
-    if let None = dbInitFlag.get() {
+    if true != *dbInitFlag.get_or_init(||{
         let r = {
             pool.transaction(|c|{
-                    // use diesel_migrations::embed_migrations;
-                    embed_migrations!();
-                    embedded_migrations::run(c).unwrap();
+                // use diesel_migrations::embed_migrations;
+                // diesel_migrations::embed_migrations!();
+                // embedded_migrations::run(c).unwrap();
                 Ok(())
             })
         };
-        dbInitFlag.set(true);
+        true
+    }) {
+        panic!("Impossible happened")
     };
 
     let res = pool.transaction(f).await;
@@ -44,10 +47,10 @@ where
 			Ok(data)
 		},
 		Err(AsyncError::Checkout(_)) => {
-			Err(crate::lib::PoolError{msg: "Pool error"})
+			Err(lib::PoolError{msg: "Pool error"})
 		},
 		Err(AsyncError::Error(_)) => {
-			Err(crate::lib::PoolError{msg: "Diesel error"})
+			Err(lib::PoolError{msg: "Diesel error"})
 		},
 	}
 }
