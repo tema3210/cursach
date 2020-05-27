9f83f04c0a15c095001e7767078d0f8798fe9aea
tema3210 <play.aab.25122001@gmail.com>
2020-05-28 02:40:53 +0300

    rewrite handler

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -7,6 +7,145 @@ use diesel::ExpressionMethods;
 use diesel::QueryDsl;
 use diesel::RunQueryDsl;
 
+#[get("/users/about/{login}")]
+pub async fn usr_about(info: web::Path<(String,)>) -> impl Responder {
+	println!("users/about handler called");
+
+	let res = lib::transaction(|conn|{
+		stmt_usr.load::<lib::ORM::UserData>(conn)
+	}).await;
+
+	match res {
+		Ok(vec) if vec.len() == 1 => {
+			let usr = &vec[0];
+			match usr.PublicProfile {
+				0 => {
+					String::from("").with_status(403.try_into().unwrap())
+				},
+				1..=2 => {
+					use serde_json::builder::ObjectBuilder;
+
+					let ut = match usr.UserType {
+						Some(1) => "Admin",
+						Some(2) => "User",
+						Some(3) => "Guest",
+						Some(_) => unreachable!(),
+						None => "NULL",
+					};
+					let mut builder = ObjectBuilder::new();
+					//Insert allowed on lvl 1 data
+					builder = builder.insert_into("UserType",ut).insert_into("About",usr.AssocInf);
+					if usr.PublicProfile == 2 {
+						//Insert allowed on lvl 2 data
+						builder = builder.insert_into("Credits",usr.Credits).insert_into("Balance",usr.Balance);
+					};
+					builder.build().to_string().with_status(200u16.try_into().unwrap())
+				},
+				_ => {
+					String::from("").with_status(500u16.try_into().unwrap())
+				}
+			}
+		},
+		Ok(vec) if vec.len() == 0 => {
+			String::from("").with_status(404u16.try_into().unwrap())
+		},
+		Ok(vec) if vec.len() > 1 => {
+			String::from("").with_status(500u16.try_into().unwrap())
+		},
+		Err(_) => {
+			String::from("").with_status(500u16.try_into().unwrap())
+		},
+	}
+
+}
+
+// #[get("/users/about/{login}")]
+// pub async fn usr_about(info: web::Path<(String,)>) -> impl Responder {
+// 	println!("users/about handler called");
+// 	use serde_json;
+//
+// 	let stmt_usr = {
+// 		use schema::UserData::dsl::*;
+// 		UserData.filter(Login.eq(Some(info.0.clone())))
+// 	};
+// 	let (res,code) = {
+// 		let res = lib::transaction(|conn|{
+// 			stmt_usr.load::<lib::ORM::UserData>(conn)
+// 		}).await;
+//
+// 		match res {
+// 			Ok(vec) => {
+// 				match vec.len() {
+// 					0 => {
+// 						(None,404)
+// 					},
+// 					1 => {
+// 						(Some(vec[0].clone()),200)
+// 					},
+// 					_ => {
+// 						(None,500)
+// 					}
+// 				}
+// 			},
+// 			Err(_) => {
+// 				(None,500)
+// 			}
+// 		}
+//
+// 	};
+// 	match code {
+// 		404 => {
+// 			String::from("").with_status(http::status::StatusCode::from_u16(404).unwrap())
+// 		},
+// 		500 => {
+// 			String::from("").with_status(http::status::StatusCode::from_u16(500).unwrap())
+// 		},
+// 		200 => {
+// 			let res = res.unwrap();//It's guranteed not to panic.
+// 			let mut new_code = code;
+// 			let t: String = {
+// 				match res.PublicProfile {
+// 					0 => {
+// 						new_code = 403;
+// 						String::from("")
+// 					},
+// 					1 => {
+// 						serde_json::json!({
+// 							"UserType": res.UserType,
+// 							//"Credits": res.Credits,
+// 							//"Balance": res.Balance,
+// 							"About": res.AssocInf,
+// 						}).to_string()
+// 					},
+// 					2 => {
+// 						let ut = match res.UserType {
+// 								Some(1) => "Admin",
+// 								Some(2) => "User",
+// 								Some(3) => "Guest",
+// 								Some(_) => unreachable!(),
+// 								None => "NULL",
+// 						};
+// 						serde_json::json!({
+// 							"UserType": ut,
+// 							"Credits": res.Credits,
+// 							"Balance": res.Balance,
+// 							"About": res.AssocInf,
+// 						}).to_string()
+// 					},
+// 					_ => {
+// 						new_code=500;
+// 						String::from("")
+// 					}
+// 				}
+//
+// 			};
+// 			t.with_status(http::status::StatusCode::from_u16(new_code).unwrap())
+// 		},
+// 		_ => unreachable!()
+// 	}
+//
+// }
+
 #[get("/users/login")]
 pub async fn usr_login(info: web::Json<lib::Protocol::UserLoginPayload>) -> impl Responder {
 	println!("users/login handler called");
@@ -48,91 +187,4 @@ pub async fn usr_login(info: web::Json<lib::Protocol::UserLoginPayload>) -> impl
 pub async fn usr_reg(info: web::Json<lib::Protocol::UserRegisterPayload>) -> impl Responder {
 	println!("users/reg handler called");
 	"Unimplemented".with_status(http::status::StatusCode::from_u16(501).unwrap())
-}
-
-#[get("/users/about/{login}")]
-pub async fn usr_about(info: web::Path<(String,)>) -> impl Responder {
-	println!("users/about handler called");
-	use serde_json;
-
-	let stmt_usr = {
-		use schema::UserData::dsl::*;
-		UserData.filter(Login.eq(Some(info.0.clone())))
-	};
-	let (res,code) = {
-		let res = lib::transaction(|conn|{
-			stmt_usr.load::<lib::ORM::UserData>(conn)
-		}).await;
-
-		match res {
-			Ok(vec) => {
-				match vec.len() {
-					0 => {
-						(None,404)
-					},
-					1 => {
-						(Some(vec[0].clone()),200)
-					},
-					_ => {
-						(None,500)
-					}
-				}
-			},
-			Err(_) => {
-				(None,500)
-			}
-		}
-
-	};
-	match code {
-		404 => {
-			String::from("").with_status(http::status::StatusCode::from_u16(404).unwrap())
-		},
-		500 => {
-			String::from("").with_status(http::status::StatusCode::from_u16(500).unwrap())
-		},
-		200 => {
-			let res = res.unwrap();//It's guranteed not to panic.
-			let mut new_code = code;
-			let t: String = {
-				match res.PublicProfile {
-					0 => {
-						new_code = 403;
-						String::from("")
-					},
-					1 => {
-						serde_json::json!({
-							"UserType": res.UserType,
-							//"Credits": res.Credits,
-							//"Balance": res.Balance,
-							"About": res.AssocInf,
-						}).to_string()
-					},
-					2 => {
-						let ut = match res.UserType {
-								Some(1) => "Admin",
-								Some(2) => "User",
-								Some(3) => "Guest",
-								Some(_) => unreachable!(),
-								None => "NULL",
-						};
-						serde_json::json!({
-							"UserType": ut,
-							"Credits": res.Credits,
-							"Balance": res.Balance,
-							"About": res.AssocInf,
-						}).to_string()
-					},
-					_ => {
-						new_code=500;
-						String::from("")
-					}
-				}
-
-			};
-			t.with_status(http::status::StatusCode::from_u16(new_code).unwrap())
-		},
-		_ => unreachable!()
-	}
-
 }
