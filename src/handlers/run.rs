8a47a8dca835d5e662184595f0333e62b93d488f
tema3210 <play.aab.25122001@gmail.com>
2020-05-27 17:19:44 +0300

    fix run handler

--- src/handlers/run.rs
+++ src/handlers/run.rs
@@ -145,13 +145,13 @@ pub async fn run_register(info: web::Json<lib::Protocol::RunRegisterPayload>) ->
 				1 => {
 					let usr = &v[0];
 					match usr.UserType {
-						Some(0) => { //guest
+						Some(lib::ORM::UserType::Guest) => { //guest
 							String::from("").with_status(403u16.try_into().unwrap())
 						},
-						Some(1) => { //user
+						Some(lib::ORM::UserType::User) => { //user
 							String::from("").with_status(403u16.try_into().unwrap())
 						}
-						Some(2) => { //admin
+						Some(lib::ORM::UserType::Admin) => { //admin
 							let horses_check = {
 								if let Some(_it) = info.competitors.iter().find(|x| **x == info.winner) {
 									Some({
